0e750c0430460a4e4048baa89bc197a9d2fbe44c
Subarna <subarna@g.harvard.edu>
2020-03-12 18:32:55 -0400

    R plot code added

--- run.sh
+++ run.sh
@@ -1,13 +1,26 @@
 #!/bin/sh  
 
-sleep_duration=3
+absolute_path=$(PWD)
+result_path="$absolute_path/results" 
+echo $result_path
+
+sleep_duration=30
 end_after=86400
 counter_arg=0
 while true  
 do  
+  now=$(date)
+  echo "\n******* Collecting data for COVID-19 at $now ******* "
+
   counter_arg=$((counter_arg+1))
-  command="python3 corona_collector.py -x $counter_arg -c USA"
-  #echo $command
-  python3 corona_collector.py -x $counter_arg -c USA		
-  sleep $sleep_duration  
+  #command="python3 corona_collector.py -x $counter_arg -c USA"
+  python3 corona_collector.py -x $counter_arg -c USA -v 2	
+  sleep 2
+
+  cd $result_path 
+  ls
+  Rscript plot.R
+  echo "plotting done! Check results at $result_path"
+  cd ..	
+  sleep $sleep_duration 
 done	
